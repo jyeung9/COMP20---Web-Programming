1e992f60d6dcce52c519c948a48b3c1ba35e8e95
Julie Yeung <jyeung@resnet140-53.medford.tufts.edu>
2019-03-05 02:17:48 -0500

    vehicles works, working on pass

---
+++ liftknockoff/liftknockoff.js
@@ -0,0 +1,291 @@
+
+			var http = new XMLHttpRequest();
+			var url = 'https://hans-moleman.herokuapp.com/rides';
+			var params = "username=cqgUgd9M&lat=42.4069238&lng=-71.1188082";
+			var min;
+			var currentMin;
+			var distance;
+			var isVehicle = false;
+
+			var all_vehicles = [];
+			var all_passengers = [];
+
+			var weiner_marker;
+			var weiner_pos;
+			var closest_weiner;
+			var currWeinerMin;
+			var weiner = {username: "", lat: 0, lng: 0};
+
+			var vehicle_marker;
+			var vehicle_pos;
+			var vehicle = {username: "", lat: 0, lng: 0};
+
+			var passenger_marker;
+			var pass_pos;
+			var passenger = {username: "", lat: 0, lng: 0};
+
+			var myLat = 0;
+			var myLng = 0;
+
+			var me = new google.maps.LatLng(myLat, myLng);
+			var myOptions = {
+				zoom: 13, // The larger the zoom number, the bigger the zoom
+				center: me,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+
+			var map;
+			var marker;
+			var infowindow = new google.maps.InfoWindow();
+
+			function init() {
+				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+				getMyLocation();
+			}
+
+			function getMyLocation() {
+				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+					navigator.geolocation.getCurrentPosition(function(position) {
+						myLat = position.coords.latitude;
+						myLng = position.coords.longitude;
+						renderMap();
+						request();
+					});
+				}
+				else {
+					alert("Geolocation is not supported by your web browser.  What a shame!");
+				}
+			}
+
+			function renderMap() {
+
+				me = new google.maps.LatLng(myLat, myLng);
+				// Update map and go there...
+				map.panTo(me);
+				
+				// Create a marker
+				console.log("my position: " + me);
+				marker = new google.maps.Marker({
+					position: me,
+					title: "Username: cqgUgd9M"
+				});
+				marker.setMap(map);
+				marker.setIcon("pusheen.png");	// customize marker
+					
+				// Open info window on click of marker
+
+				for(i = 0; i < all_vehicles.length; i++){
+					if(all_vehicles[i].vehicle){
+
+						vehicle_pos = new google.maps.LatLng(all_vehicles[i].vehicle.lat, all_vehicles[i].vehicle.lng);
+						vehicle_marker = new google.maps.Marker({
+							position: vehicle_pos,
+							title: "hi"
+						});
+						vehicle_marker.setMap(map);
+						vehicle_marker.setIcon("car.png");
+					}
+
+					google.maps.event.addListener(vehicle_marker, 'click', function() {
+							infowindow.setContent(vehicle_marker.title);
+							infowindow.open(map, vehicle_marker);
+						});
+				}
+			}
+
+			function request(){
+
+				params = "username=cqgUgd9M&lat=" + myLat + "&lng=" + myLng;
+				http.open('POST',url,true);
+				//http.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+				http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	
+				http.onreadystatechange = function(){
+
+					if(http.readyState == 4 && http.status == 200) {
+						console.log("here i am 4");
+						parsedMessage = JSON.parse(http.responseText);	// array of vehicles 
+	
+						alert(http.responseText);
+							// push objects to appropriate arrays
+							if(parsedMessage["vehicles"]){
+								isVehicle = true;
+								console.log("entered vehicles");
+								for (i = 0; i < parsedMessage["vehicles"].length; i++){
+									if (parsedMessage["vehicles"][i].username == "WEINERMOBILE"){
+										weiner = {username: "WEINERMOBILE", lat: parsedMessage["vehicles"][i].lat, lng: parsedMessage["vehicles"][i].lng};
+
+										all_vehicles.push({
+											weiner
+										})				
+										renderWeiner();
+									}
+									else {
+										vehicle = {username: parsedMessage["vehicles"][i].username, lat: parsedMessage["vehicles"][i].lat, lng: parsedMessage["vehicles"][i].lng};
+										all_vehicles.push({
+											vehicle
+										})
+										renderVehicle();
+									}
+								}
+
+								// finding closest vehicle, weinermobile ---------------------------
+
+								closest_weiner = checkDistance(parsedMessage["vehicles"][0].lat, parsedMessage["vehicles"][0].lng);
+								min = checkDistance(parsedMessage["vehicles"][0].lat, parsedMessage["vehicles"][0].lng);
+
+								for (count = 1; count < parsedMessage["vehicles"].length; count++) {
+									if(parsedMessage["vehicles"].username == "WEINERMOBILE"){
+										currWeinerMin = checkDistance(parsedMessage["vehicles"][count].lat, parsedMessage["vehicles"][count].lng);
+										if(currentWeinerMin < closest_weiner){
+											closest_weiner = currentWeinerMin;
+										}
+									}
+						
+									currentMin = checkDistance(parsedMessage["vehicles"][count].lat, parsedMessage["vehicles"][count].lng);
+									if (currentMin < min){
+										min = currentMin;
+									}
+								}
+
+								google.maps.event.addListener(marker, 'click', function() {
+									infowindow.setContent(this.title + " Distance from closest vehicle: " + min + " Distance from Closest Weinermobile: " + closest_weiner);
+									infowindow.open(map, this);
+								});
+							}
+
+							// handling passenger case
+							if(parsedMessage["passengers"]){
+
+								isVehicle = false;
+								console.log("entered passengers");
+
+								for (j = 0; j < parsedMessage["passengers"].length; j++){
+									if (parsedMessage["passengers"][j].username == "WEINERMOBILE"){
+										weiner = {username: "WEINERMOBILE", lat: parsedMessage["passengers"][j].lat, lng: parsedMessage["passengers"][j].lng};
+
+										all_vehicles.push({
+											weiner
+										})				
+										renderWeiner();
+									}
+									else{
+
+										passenger = {username: parsedMessage["passengers"][j].username, lat: parsedMessage["passengers"][j].lat, lng: parsedMessage["passengers"][j].lng};
+										console.log(passenger.username);
+										console.log(passenger.lat);
+										console.log(passenger.lng);
+										all_passengers.push({
+											passenger
+										})
+										console.log(passenger.username);
+										renderPassenger();
+
+									}
+								}
+
+								// finding closest weinermobile, passenger
+								closest_weiner = checkDistance(parsedMessage["passengers"][0].lat, parsedMessage["passengers"][0].lng);
+								min = checkDistance(parsedMessage["passengers"][0].lat, parsedMessage["passengers"][0].lng);
+
+								for (count = 1; count < parsedMessage["passengers"].length; count++) {
+									if(parsedMessage["passengers"].username == "WEINERMOBILE"){
+										currWeinerMin = checkDistance(parsedMessage["passengers"][count].lat, parsedMessage["passengers"][count].lng);
+										if(currentWeinerMin < closest_weiner){
+											closest_weiner = currentWeinerMin;
+										}
+									}
+						
+									currentMin = checkDistance(parsedMessage["passengers"][count].lat, parsedMessage["passengers"][count].lng);
+									if (currentMin < min){
+										min = currentMin;
+									}
+								}
+
+								google.maps.event.addListener(marker, 'click', function() {
+									infowindow.setContent(this.title + " Distance from closest passenger: " + min + " Distance from Closest Weinermobile: " + closest_weiner);
+									infowindow.open(map, this);
+								});
+							}
+					}
+				}
+				http.send(params);
+			}
+
+			//--------------------------
+			function renderWeiner() {
+				console.log("inside renderweiner");
+				weiner_pos = new google.maps.LatLng(all_vehicles[i].weiner.lat, all_vehicles[i].weiner.lng);
+				map.panTo(weiner_pos);
+				weiner_marker = new google.maps.Marker({
+					position: weiner_pos,
+					title: "Username: WEINERMOBILE" + " Distance from me: " + checkDistance(all_vehicles[i].weiner.lat, all_vehicles[i].weiner.lng)
+				});
+
+				weiner_marker.setMap(map);
+				weiner_marker.setIcon("weinermobile.png");
+
+				google.maps.event.addListener(weiner_marker, 'click', function() {
+					infowindow.setContent(this.title);
+					infowindow.open(map, this);
+				});
+			}
+
+			function renderVehicle() {
+
+				vehicle_pos = new google.maps.LatLng(all_vehicles[i].vehicle.lat, all_vehicles[i].vehicle.lng);
+				//map.panTo(vehicle_pos);
+				vehicle_marker = new google.maps.Marker({
+					position: vehicle_pos,
+					title: "Username: " + all_vehicles[i].vehicle.username + " Distance from me: " + checkDistance(all_vehicles[i].vehicle.lat, all_vehicles[i].vehicle.lng)
+				});
+
+				vehicle_marker.setMap(map);
+				vehicle_marker.setIcon("car.png");
+
+				google.maps.event.addListener(vehicle_marker, 'click', function() {
+					infowindow.setContent(this.title);
+					infowindow.open(map, this);
+				});
+			}
+
+			function renderPassenger(){
+				console.log("inside render pass");
+				console.log(all_passengers[j].passenger.lat);
+				pass_pos = new google.maps.LatLng(all_passengers[j].passenger.lat, all_passengers[j].passenger.lng);
+				//map.panTo(pass_pos);
+				passenger_marker = new google.maps.Marker({
+					position: pass_pos,
+					title: "Username: " + all_passengers[j].passenger.username + " Distance from me: " + checkDistance(all_passengers[j].passenger.lat, all_passengers[j].passenger.lng)
+				});
+
+				passenger_marker.setMap(map);
+				passenger_marker.setIcon("pass_pusheen.png");
+
+				google.maps.event.addListener(passenger_marker, 'click', function() {
+					console.log("entered pass");
+					infowindow.setContent(this.title);
+					infowindow.open(map, this);
+				});
+
+			}
+			
+			function checkDistance(other_lat, other_lng){
+				Number.prototype.toRad = function() {
+				   return this * Math.PI / 180;
+				}
+
+				var R = 3958.756 // 6371, km 
+				//has a problem with the .toRad() method below.
+				var x1 = myLat - other_lat;
+				var dLat = x1.toRad();  
+				var x2 = myLng - other_lng;
+				var dLon = x2.toRad();  
+				var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+				        Math.cos(myLat.toRad()) * Math.cos(other_lat.toRad()) * 
+				        Math.sin(dLon/2) * Math.sin(dLon/2);  
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+				distance = R * c; 
+				return distance;
+
+			}
